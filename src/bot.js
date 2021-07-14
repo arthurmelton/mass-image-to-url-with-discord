c344e7ae883778bd5e9e7cdc8f1d58d946e6a465
Arthur Melton <amtitan@icloud.com>
2021-07-13 20:12:47 -0400

    working

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-// See if config file exists
 try {
     if (fs.existsSync('Config.json')) {
         console.log("The file exists dont need to make it.");
@@ -8,9 +7,9 @@ try {
         console.log('The file does not exist so trying to make it.');
         fs.writeFileSync('Config.json', "{\n" +
             "  \"Token\": \"(discord bot token)\",\n" +
-            "  \"Your Discord Id\": \"(id)\",\n" +
-            "  \"Image Folder Path\": \"(path)\",\n" +
-            "  \"OutPut Path\": \"(path)\"\n" +
+            "  \"Your_Discord_Id\": \"(id)\",\n" +
+            "  \"Image_Folder_Path\": \"(path)\",\n" +
+            "  \"OutPut_Path\": \"(path)\"\n" +
             "}", function(err) {
             if (err) {
                 console.error(err);
@@ -26,11 +25,23 @@ try {
 }
 
 const config = JSON.parse(fs.readFileSync("Config.json"));
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageAttachment } = require('discord.js');
+const client = new Client();
+
+fs.writeFileSync(config.OutPut_Path, "")
 
 client.on('ready', () => {
-    
+    fs.readdir(config.Image_Folder_Path, (err, files) => {
+        files.forEach(file => {
+            const attachment = new MessageAttachment(`${config.Image_Folder_Path}/${file}`);
+            client.users.cache.get(config.Your_Discord_Id).send(attachment)
+                .then(function(messageSent) {
+                    console.log(messageSent.attachments.first().url);
+                    fs.writeFileSync(config.OutPut_Path, `${fs.readFileSync(config.OutPut_Path)}\n${messageSent.attachments.first().url}`);
+                })
+                .catch(console.error);
+        })
+    })
 });
 
 client.login(config.Token);
